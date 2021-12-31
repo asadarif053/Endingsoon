49c2cee97d74fc671387917dda06ec3fe8ace793
Asaduzzaman <asadarif053@gmail.com>
2021-12-31 18:17:11 +0600

    version 1.0

--- ViewModel/ProductMessage.php
+++ ViewModel/ProductMessage.php
@@ -1,12 +1,25 @@
 <?php
 namespace Asad\Endingsoon\ViewModel;
 use Magento\Framework\View\Element\Block\ArgumentInterface; //require to mark as viewmodel
+use Asad\Endingsoon\Helper\Data;
 
 Class ProductMessage implements ArgumentInterface
 {
 
+    protected $helperData;
+
+	public function __construct(
+		Data $helperData
+
+	)
+	{
+		$this->helperData = $helperData;
+	}
+
     public function getMessage(): string
     {
-        return "Hello from the viewModel";
+
+        //return 'asdf asdf';
+        return $this->helperData->getGeneralConfig('delivery_msg');
     }
 }
--- ViewModel/Stock.php
+++ ViewModel/Stock.php
@@ -3,16 +3,24 @@ namespace Asad\Endingsoon\ViewModel;
 use Magento\CatalogInventory\Api\StockStateInterface;
 use Magento\Catalog\Block\Product\View\AbstractView;
 use Magento\Framework\View\Element\Block\ArgumentInterface; //require to mark as viewmodel
+use Asad\Endingsoon\Helper\Data;
 
 Class Stock implements ArgumentInterface
 {
 
     private $stockState,$productview;
+    protected $helperData;
 
-    public function __construct(StockStateInterface $stockConfiguration, AbstractView $product)
+    public function __construct(
+        StockStateInterface $stockConfiguration, 
+        AbstractView $product,
+        Data $helperData
+    )
     {
         $this->stockState = $stockConfiguration;
         $this->productview = $product;
+        $this->helperData = $helperData;
+        
     }
 
     public function getProduct(){
@@ -22,7 +30,14 @@ Class Stock implements ArgumentInterface
     public function getStatus(): string
     {
         $qty = $this->getStockQty($this->getProduct()->getId());
-        return "{$qty} items left";
+        $tqty = $this->helperData->getGeneralConfig('qty_threshold');
+        $tqtyMsg = $this->helperData->getGeneralConfig('threshold_msg');
+
+        if($qty <= (int)$tqty)
+           return "{$qty} $tqtyMsg";
+
+        else 
+            return 'Available';  
     }
 
     public function getStockQty($productId, $websiteId = null): int
