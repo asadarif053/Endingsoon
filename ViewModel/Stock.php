a54da040815f710f43e35d5384906caf74c0bd11
Asaduzzaman <asadarif053@gmail.com>
2021-12-31 17:01:00 +0600

    Qty retrieved

--- ViewModel/Stock.php
+++ ViewModel/Stock.php
@@ -1,13 +1,32 @@
 <?php
 namespace Asad\Endingsoon\ViewModel;
+use Magento\CatalogInventory\Api\StockStateInterface;
+use Magento\Catalog\Block\Product\View\AbstractView;
 use Magento\Framework\View\Element\Block\ArgumentInterface; //require to mark as viewmodel
 
 Class Stock implements ArgumentInterface
 {
 
+    private $stockState,$productview;
+
+    public function __construct(StockStateInterface $stockConfiguration, AbstractView $product)
+    {
+        $this->stockState = $stockConfiguration;
+        $this->productview = $product;
+    }
+
+    public function getProduct(){
+        return $this->productview->getProduct();
+    }
+
     public function getStatus(): string
     {
+        $qty = $this->getStockQty($this->getProduct()->getId());
+        return "{$qty} items left";
+    }
 
-        return "Ending Soon!";
+    public function getStockQty($productId, $websiteId = null): int
+    {
+        return $this->stockState->getStockQty($productId, $websiteId);
     }
 }
